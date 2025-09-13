24267c9ae1da6c9cc15cabfa341a1a89828e8c56
Xiaoling Cui <10430@holbertonstudents.com>
2025-09-13 23:48:10 +1000

    Add comments

--- task3/back-end/api.py
+++ task3/back-end/api.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
+# CORS allows web app running on one origin to safely request resources from a different origin
 CORS(app)
+
 
 @app.route('/api/hello')
 def hello_world():
     return 'Hello, World!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5252)
